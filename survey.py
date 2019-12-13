b62d156ee0cf80fad6e3e98f2def163429b03c12
alexey zotov <zotov.alexei.1@yandex.ru>
2019-12-13 21:10:02 +0300

    Feature: add popularity charts, add linear regression

--- survey.py
+++ survey.py
@@ -1,6 +1,22 @@
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
+from sklearn.linear_model import LinearRegression
+from statistics import median
+
+
+def get_good_reviews(data):
+    return [len(list(filter(lambda x: x in ["Планирую заниматься", "Занимался, в основном положительный опыт",
+                                            "Занимался, все понравилось"], review))) for review in data]
+
+
+def get_prediction(x, y):
+    X = pd.Series(x).values.reshape(-1, 1)
+    Y = pd.Series(y).values.reshape(-1, 1)
+    linear_regressor = LinearRegression()
+    linear_regressor.fit(X, Y)
+    Y_pred = linear_regressor.predict(X)
+    return X, Y, Y_pred
 
 
 def draw_pie_chart(data, title):
@@ -60,6 +76,61 @@ def draw_horizontal_bars(data):
     #plt.show()
 
 
+def draw_popularity_charts(platforms, data):
+    # для каждой платформы возьмем медианный отзыв, кроме "не слышал"
+    opinions_to_numbers = {"Абсолютно неинтересно": -3, "Интересно, но отталкивает стоимость": -2,
+                         "Занимался, но не понравилось": -1, "Планирую заниматься": 1,
+                         "Занимался, в основном положительный опыт": 2, "Занимался, все понравилось": 3}
+
+    reviews = [sorted([opinions_to_numbers[x] for x in review if x != "Не слышал"]) for review in data]
+    median_reviews = list(map(median, reviews))
+
+    # сортируем платформы по полученному медианному значению
+    sorted_data = sorted(zip(platforms, median_reviews), key=lambda x: x[1])
+
+    keys = [x[0] for x in sorted_data]
+    values = [x[1] for x in sorted_data]
+
+    plt.figure(figsize=(15, 10))
+    plt.ylim(-4, 3)
+    plt.scatter(keys, values)
+    plt.yticks(range(-3, 4), opinions_to_numbers.keys())
+    plt.xlabel("Название платформы")
+    plt.title("Медианная оценка платформы")
+
+    # сделаем линейную регрессию
+    platforms_indices = range(len(platforms))
+    X, Y, pred = get_prediction(platforms_indices, values)
+
+    plt.scatter(X, Y)
+    plt.plot(X, pred)
+
+    plt.savefig("output/Медианная оценка платформы", bbox_inches="tight")
+    #plt.show()
+
+    # теперь посчитаем долю хороших отзывов от всех отзывов
+    good_reviews_share = [x / 40 for x in get_good_reviews(data)]
+    sorted_data = sorted(zip(platforms, good_reviews_share), key=lambda x: x[1])
+
+    keys = [x[0] for x in sorted_data]
+    values = [x[1] for x in sorted_data]
+
+    plt.figure(figsize=(15, 10))
+    plt.ylim(0, 0.5)
+    plt.scatter(keys, values)
+    plt.xlabel("Название платформы")
+    plt.title("Доля положительных отзывов платформы")
+
+    # сделаем линейную регрессию
+    platforms_indices = range(len(platforms))
+    X, Y, pred = get_prediction(platforms_indices, values)
+
+    plt.scatter(X, Y)
+    plt.plot(X, pred)
+    plt.savefig("output/Доля положительных отзывов", bbox_inches="tight")
+    #plt.show()
+
+
 def parse_data(data):
     questions = data.columns[1:]
     answers = []
@@ -82,21 +153,19 @@ def parse_data(data):
             draw_pie_chart(answers[i], titles[i])
 
     platforms = ["Coursera", "Stepik", "Edx", "GeekBrains", "Udacity", "Udemy", "Открытое образование", "Фоксфорд"]
-    good_reviews_number = [len(list(filter(lambda x: x in ["Планирую заниматься", "Занимался, в основном положительный опыт",
-                                            "Занимался, все понравилось"], review))) for review in answers[4:12]]
+    good_reviews_number = get_good_reviews(answers[4:12])
 
     # сортируем платформы по количеству положительных отзывов
-    data = sorted(zip(platforms, good_reviews_number), key=lambda x: x[1])
-    draw_horizontal_bars(data)
+    sorted_data = sorted(zip(platforms, good_reviews_number), key=lambda x: x[1])
+    draw_horizontal_bars(sorted_data)
 
-    return answers
+    draw_popularity_charts(platforms, answers[4:12])
 
 
 def main():
     filename = "online_courses.csv"
     data = pd.read_csv(filename)
-    result = parse_data(data)
-    #print(result)
+    parse_data(data)
 
 
 if __name__ == "__main__":
