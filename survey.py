a736cf95838d78e3d31f3a80e3735b5939f53e8d
alexey zotov <zotov.alexei.1@yandex.ru>
2019-12-13 19:08:40 +0300

    Feature: add bar chart Fix: simplify horizontal bar chart

--- survey.py
+++ survey.py
@@ -11,12 +11,11 @@ def draw_pie_chart(data, title):
     keys = list(options.keys())
     values = list(options.values())
 
-    plt.rcdefaults()
     fig, ax = plt.subplots(figsize=(15, 10), subplot_kw=dict(aspect="equal"))
 
     def func(pct, allvals):
         absolute = int(pct / 100. * np.sum(allvals))
-        return "{:.1f}%\n({:d})".format(pct, absolute)
+        return "{:.1f}%".format(pct, absolute)
 
     wedges, texts, autotexts = ax.pie(values, autopct=lambda pct: func(pct, values),
                                       textprops=dict(color="w"))
@@ -31,25 +30,33 @@ def draw_pie_chart(data, title):
 
 
 def draw_bar_chart(data, title):
-    pass
+    options = {option: 0 for option in set(data)}
+    for item in data:
+        options[item] = options.get(item, 0) + 1
+
+    keys = list(options.keys())
+    values = list(options.values())
+
+    plt.figure(figsize=(15, 10))
+    plt.bar(keys, values, width=0.3)
+    plt.title("Лучшие предметы для изучения онлайн")
+
+    plt.savefig("output/{}".format(title), bbox_inches="tight")
+    #plt.show()
 
 
 def draw_horizontal_bars(data):
     platforms = [x[0] for x in data]
     good_reviews = [x[1] for x in data]
 
-    plt.rcdefaults()
-    fig, ax = plt.subplots(figsize=(15, 10), subplot_kw=dict(aspect="equal"))
-
+    plt.figure(figsize=(15, 10))
     y_pos = np.arange(len(platforms))
+    plt.barh(y_pos, good_reviews, align="center")
+    plt.yticks(y_pos, platforms)
+    plt.xlabel("Количество положительных отзывов")
+    plt.title("Популярность разных платформ")
 
-    ax.barh(y_pos, good_reviews, align='center')
-    ax.set_yticks(y_pos)
-    ax.set_yticklabels(platforms)
-    ax.set_xlabel('Количество положительных отзывов')
-    ax.set_title('Популярность разных платформ')
-
-    plt.savefig("output/Популярность платформ")
+    plt.savefig("output/Популярность платформ", bbox_inches="tight")
     #plt.show()
 
 
@@ -61,7 +68,7 @@ def parse_data(data):
         cur_answers = data[question]
         answers.append(list(cur_answers))
 
-    answers[-6] = ";".join(answers[-6]).split(";")
+    answers[-6] = [x[:x.find(" (")] for x in ";".join(answers[-6]).split(";")]  # нормализуем ответ с multiple choice
 
     titles = ["Пол", "Образование", "Тип населенного пункта", "Опыт онлайн-обучения", "Coursera", "Stepik",
               "Edx", "GeekBrains", "Udacity", "Udemy", "Открытое образование", "Фоксфорд", "Важнейший критерий",
@@ -69,8 +76,10 @@ def parse_data(data):
               "Смотрят ли работодатели на сертификаты", "Сможет ли заменить очное образование"]
 
     for i in range(len(answers)):
-        if not 4 <= i <= 11:  # вопросы, по которым строить pie chart
-            draw_pie_chart(answers[i], titles[i])  # вопросы, по которым строить pie charts
+        if i == 13:  # вопрос, по которому строить bar chart
+            draw_bar_chart(answers[i], titles[i])
+        elif not 4 <= i <= 11:  # вопросы, по которым строить pie chart
+            draw_pie_chart(answers[i], titles[i])
 
     platforms = ["Coursera", "Stepik", "Edx", "GeekBrains", "Udacity", "Udemy", "Открытое образование", "Фоксфорд"]
     good_reviews_number = [len(list(filter(lambda x: x in ["Планирую заниматься", "Занимался, в основном положительный опыт",
@@ -87,7 +96,7 @@ def main():
     filename = "online_courses.csv"
     data = pd.read_csv(filename)
     result = parse_data(data)
-    print(result)
+    #print(result)
 
 
 if __name__ == "__main__":
